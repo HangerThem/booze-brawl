47df32a4e7114d574b512ff8c4daa6a3b87c6170
HangerThem <f.borisjuk@gmail.com>
2023-12-24 16:22:04 +0100

    First actuall data

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,136 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+
+import { useState, useEffect } from "react";
+import styled from "styled-components";
+import prompts from "@/data/data.json";
+import ShotGlassIcon from "@/icons/shotGlassIcon";
+
+interface CardProps {
+  termination: string;
+}
+
+interface CardDeckProps {
+  height: number;
+}
+
+const Card = styled.div<CardProps>`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 300px;
+  height: 400px;
+  font-size: 15px;
+  background-color: ${(props) => {
+    return props.termination === "true" ? "#DD0000" : "#000000";
+  }};
+  box-shadow: ${(props) => {
+    return props.termination === "true" ? "0px 0px 50px 4px #AA0000" : "none";
+  }};
+  border-radius: 8px;
+  padding: 16px;
+  color: white;
+
+  transition: transform 0.5s;
+  transform-style: preserve-3d;
+
+  &:hover {
+    cursor: pointer;
+    transform: scale(1.1) rotateY(10deg) rotateX(10deg);
+  }
+
+  h2 {
+    margin: 0;
+  }
+
+  p {
+    display: ${(props) => {
+      return props.termination === "true" ? "none" : "flex";
+    }};
+    align-self: flex-end;
+    margin: 0;
+    font-size: 20px;
+    gap: 8px;
+    font-weight: bold;
+
+    svg {
+      margin-right: 8px;
+
+      path {
+        fill: white;
+      }
+    }
+  }
+`;
+
+const PageContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 16px;
+  height: 100vh;
+  justify-content: center;
+  background-color: #070707;
+`;
 
 export default function Home() {
+  const [data, setData] = useState<any>(prompts);
+  const [prompt, setPrompt] = useState<any>(null);
+
+  const drawPrompt = () => {
+    const random = Math.floor(Math.random() * data.length);
+    const prompt = data[random];
+    setData(data.filter((item: any) => item !== prompt));
+    return prompt;
+  };
+
+  useEffect(() => {
+    setPrompt(drawPrompt());
+
+    window.addEventListener("keydown", (e) => {
+      if (e.key === " ") {
+        setPrompt(drawPrompt());
+      }
+    });
+  }, []);
+
+  const reset = () => {
+    setData(prompts);
+  };
+
+  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    const el = document.querySelector<HTMLDivElement>("div[termination]");
+    if (el) {
+      const x = e.clientX - window.innerWidth / 2;
+      const y = e.clientY - window.innerHeight / 2;
+      const rotateY = Math.min(Math.max(x / 10, -10), 10);
+      const rotateX = Math.min(Math.max(y / -10, -10), 10);
+      el.style.transform = `perspective(1500px) 
+                            rotateX(${rotateX}deg) 
+                            rotateY(${rotateY}deg) 
+                            scale3d(1, 1, 1)`;
+    }
+  };
+
+  useEffect(() => {
+    if (data.length === 0) {
+      reset();
+    }
+  }, [data]);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+    <PageContainer onMouseMove={(e) => handleMouseMove(e)}>
+      {prompt && (
+        <Card
+          termination={prompt.termination ? "true" : "false"}
+          onClick={() => setPrompt(drawPrompt())}
         >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
+          <h2>{prompt.question}</h2>
           <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            {prompt.shots}
+            <ShotGlassIcon />
           </p>
-        </a>
-      </div>
-    </main>
-  )
+        </Card>
+      )}
+    </PageContainer>
+  );
 }
---
+++ styles/global.ts
@@ -0,0 +1,18 @@
+"use client";
+
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  ::selection {
+    background-color: none;
+    color: none;
+  }
+`;
